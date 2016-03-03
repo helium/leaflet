70b5cd4c0a1777cd1b85e2b51790c726bd558151
Reid Draper <reiddraper@gmail.com>
2016-03-03 14:55:55 -0600

    Add a little TBQueue benchmark

--- app/Main.hs
+++ app/Main.hs
@@ -2,5 +2,24 @@ module Main where
 
 import Lib
 
+import Control.Monad
+import Control.Concurrent.Async
+import Control.Concurrent.MVar
+import Control.Concurrent.STM
+
+numMessages :: Int
+numMessages = 1000 * 1000 * 1000
+
+messages :: [Int]
+messages = [1..numMessages]
+
+go :: IO ()
+go = do
+    queue <- newTBQueueIO 100 :: IO (TBQueue Int)
+    producer <- async (mapM_ (atomically . writeTBQueue queue) messages)
+    consumer <- async (replicateM_ numMessages (atomically (readTBQueue queue)))
+    wait producer >>= print
+    wait consumer >>= print
+
 main :: IO ()
-main = newRouter >> return ()
+main = async go >>= wait >>= print
